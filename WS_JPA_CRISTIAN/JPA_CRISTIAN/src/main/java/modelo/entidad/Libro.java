49be0e3c8e00aad5311c2148575514e52a2aa938
Cristian.Quiceno <cristian.qprog@gmail.com>
2024-02-26 15:14:12 +0100

    v.1.0

---
+++ WS_JPA_CRISTIAN/JPA_CRISTIAN/src/main/java/modelo/entidad/Libro.java
@@ -0,0 +1,111 @@
+package modelo.entidad;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String titulo;
+	private double precio;
+	
+	//Autor
+	@ManyToOne
+	@JoinColumn(name="fk_id_autor", referencedColumnName="id")
+	private Autor autor;
+	
+	//editorial
+	@ManyToOne
+	@JoinColumn(name="fk_id_editorial", referencedColumnName="id")
+	private Editorial editorial;
+	
+	//lIBRERÍA
+	@ManyToMany(mappedBy="libro", cascade=CascadeType.PERSIST) 
+	private List<Libreria> libreria;
+
+	public Libro() {
+		super();
+	}
+
+	public Libro(Integer id, String titulo, double precio, Autor autor, Editorial editorial) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.precio = precio;
+		this.autor = autor;
+		this.editorial = editorial;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	public Editorial getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(Editorial editorial) {
+		this.editorial = editorial;
+	}
+
+	public List<Libreria> getLibrerias() {
+		return libreria;
+	}
+
+	public void setLibrerias(List<Libreria> librerias) {
+		this.libreria = librerias;
+	}
+
+	@Override
+	public String toString() {
+		return "Libro [id=" + id + ", titulo=" + titulo + ", precio=" + precio + ", autor=" + autor + ", editorial="
+				+ editorial + ", librerias=" + libreria + "]";
+	}
+	
+	
+	
+	
+
+}
